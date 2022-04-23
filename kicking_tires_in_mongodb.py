451f546584cf54d719df1d36b5a4101d26ab3c2c
Ashmaawyy <al_ashmawy@outlook.com>
2022-04-23 13:39:19 +0200

    Code is now clean and shiny : )

--- kicking_tires_in_mongodb.py
+++ kicking_tires_in_mongodb.py
@@ -1,43 +1,64 @@
+from gridfs import Database
+from matplotlib.collections import Collection
 from pymongo import MongoClient
 from pymongo import errors
 import sys
 import pandas as pd
+
 atlas_creds = pd.read_csv('mongo_atlas_creds.csv')
 
-def add_city(db):
-    # Changes to this function will be reflected in the output. 
-    # All other functions are for local use only.
-    # Try changing the name of the city to be inserted
+def get_clean_city_data():
+    """
+    A function to clean city data :)
+    Returns a clean dictionary of city data.
+    """
+
+    cities_df = pd.read_csv('cities.csv')
+    cities_dict = cities_df.to_dict()
+    return cities_dict
+
+def add_cities_to_db(db: Database, cities_data: dict):
+    """
+    A function to insert city data into db
+    Arguments: db -> collection object, cities_data -> dictionary
+    """
+
     try:
-        db.cities.insert_one({"name" : "Chicago",
-                              "long" : "41.8781 N",
-                              "lat" : "87.6298 W"})
+        db.cities.insert_many(cities_data)
         print('Inserted data into database successfully :)')
     
     except(errors.ServerSelectionTimeoutError):
         print("Oops! insert took too long")
     
-def get_city(db):
+def fetch_first_row(db: Database):
+    """
+    A function to print the first row in our data to test that data is correct
+    Arguments: db -> collection object
+    """
+
     try:
         return db.cities.find_one()
-        print('Query completed successfully :)')
 
     except(errors.ServerSelectionTimeoutError):
         print("Oops! looks like your query took too long")
 
 def get_db():
+    """
+    A function to create the database on a MongoDB cluster
+    """
+    
     try:
         client = MongoClient("mongodb+srv://Ashmawy:{}@cluster0.ta7o8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(atlas_creds['value'][1]))
         db = client.cities
 
         print('Connected Ok :)')
-    # 'examples' here is the database name. It will be created if it does not exist.
-    except(TimeoutError):
+
+    except(errors.ServerSelectionTimeoutError):
         print("Oops! connection timed out")
 
     return db
 
 if __name__ == "__main__":
     db = get_db()
-    add_city(db)
-    print(get_city(db))
+    add_cities_to_db(db, get_clean_city_data())
+    fetch_first_row(db)
