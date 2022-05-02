a7ebb50a7286d90d6d86d6190a0b4bcd385f4163
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-02 22:08:20 +0200

    still trying to make a dictionary from osm data :)

--- osm_data_handler.py
+++ osm_data_handler.py
@@ -6,15 +6,17 @@ import pandas as pd
 file_name = 'giza_osm_data.xml'
 keys = []
 values = []
-#giza_osm_dict = collections.defaultdict(list)
+giza_osm_dict = collections.defaultdict(list)
 #giza_osm_df = pd.read_xml(file_name)
-#for event, elem in ET.iterparse(file_name):
-#    keys.append(elem.tag)
-#    values.append(elem.attrib)
+for event, elem in ET.iterparse(file_name, events = ('start',)):
+    keys.append(elem.tag)
+    values.append(elem.attrib)
 
 #for key in keys:
 #    giza_osm_dict[key].append(str(values[i]) for i in range(len(values)))
+#    print('adding value to key: ', key, end = '\r')
 #giza_osm_dict = {keys[i]: values[i] for i in range(len(keys))}
-#pprint.pprint(giza_osm_df.head(10))
+#pprint.pprint(giza_osm_dict)
 #print('Lengh of the generated dict is: ', len(giza_osm_dict))
+print('First 10 keys are: ', keys[:10], '\n', 'The first 10 values are: ', values[:10])
 print('Lengh of keys is: ', len(keys),', ', 'Lengh of values is: ', len(values))
