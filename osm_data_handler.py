e743377ea5a3e28a49cebe4d3890bc5337cd73bd
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-03 05:30:49 +0200

    I hope we found the best way : )

--- osm_data_handler.py
+++ osm_data_handler.py
@@ -1,14 +1,15 @@
 import xml.etree.ElementTree as ET
 import pprint
+import collections
 
 file_name = 'giza_osm_data.xml'
 keys = set()
 values = []
-giza_osm_dict = {}
+giza_osm_dict = collections.defaultdict(list)
 
 for event, elem in ET.iterparse(file_name, events = ('start',)):
-    giza_osm_dict[elem.tag] = [elem.attrib].append(elem.attrib)
+    giza_osm_dict[elem.tag].append(elem.attrib)
     print('adding attribute: ', elem.attrib, ' to key: ', elem.tag)
 
-pprint.pprint(list(giza_osm_dict.items())[:11])
+pprint.pprint(giza_osm_dict)
 print('Lengh of the generated dict is: ', len(giza_osm_dict))
