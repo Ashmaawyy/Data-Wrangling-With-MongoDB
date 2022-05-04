ab03d773345dbf6128571e8233ae598cad0d600b
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-04 08:03:35 +0200

    Trying to make the best way better : )

--- osm_data_handler.py
+++ osm_data_handler.py
@@ -4,12 +4,12 @@ import collections
 
 file_name = 'giza_osm_data.xml'
 keys = set()
-values = []
 giza_osm_dict = collections.defaultdict(list)
 
 for event, elem in ET.iterparse(file_name, events = ('start',)):
     giza_osm_dict[elem.tag].append(elem.attrib)
 
+pprint.pprint(giza_osm_dict['member'])
 print(
     'Number of nodes found: ', len(giza_osm_dict['node']), '\n',
     'Number of members found: ', len(giza_osm_dict['member']), '\n',
