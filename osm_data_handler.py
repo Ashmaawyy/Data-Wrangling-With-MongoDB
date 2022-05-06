0fcd6ec406872d6110cf3f17d9fcc70e2d8b259b
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-06 22:09:29 +0200

    We now can extract ways : )

--- osm_data_handler.py
+++ osm_data_handler.py
@@ -6,20 +6,22 @@ import re
 file_name = 'giza_osm_data.xml'
 street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 giza_osm_dict = collections.defaultdict(list)
-street_types = collections.defaultdict(set)
+streets = collections.defaultdict(set)
 
 def build_main_dict():
     for event, elem in ET.iterparse(file_name, events = ('start',)):
-        giza_osm_dict[elem.tag].append(elem.attrib)
+        streets = audit_street_name(elem)
+    for street in streets.values():
+        giza_osm_dict['ways'].append(street)
 
-    return giza_osm_dict
     print(
         '\n',
-        'Number of nodes found: ', len(giza_osm_dict['node']), '\n',
-        'Number of members found: ', len(giza_osm_dict['member']), '\n',
-        'Number of relations found: ', len(giza_osm_dict['relation']), '\n',
-        'Number of ways found: ', len(giza_osm_dict['way']),
+        'Number of nodes found: ', len(giza_osm_dict['nodes']), '\n',
+        'Number of members found: ', len(giza_osm_dict['members']), '\n',
+        'Number of relations found: ', len(giza_osm_dict['relations']), '\n',
+        'Number of ways found: ', len(giza_osm_dict['ways']),
         '\n')
+    return giza_osm_dict
 
 def audit_street_type(street_types, street_name):
     match = street_type_re.search(street_name)
@@ -30,13 +32,13 @@ def audit_street_type(street_types, street_name):
 def is_street_name(elem):
     return (elem.attrib['k'] == 'addr:street')
 
-def audit_street_name():
-    for event, elem in ET.iterparse(file_name, events = ('start',)):
+def audit_street_name(elem):
         if elem.tag == 'way':
             for tag in elem.iter('tag'):
                 if is_street_name(tag):
-                    audit_street_type(street_types, tag.attrib['v'])
-    pprint.pprint(dict(street_types))
+                    audit_street_type(streets, tag.attrib['v'])
+
+        return streets
 
 if __name__ == '__main__':
-    audit_street_name()
+    pprint.pprint(build_main_dict())
